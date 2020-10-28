ae1c7d9135ef87f02720d98ed8f9a593d0321a8c
isFernandes <iago.stosfernandes@gmail.com>
2020-10-28 11:18:36 -0300

    structure landing page

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./index.css";
+import Landing from "./pages/Landing";
+
+function App() {
+  return (
+    <div id="App">
+      <Landing />
+    </div>
+  );
+}
+
+export default App;
