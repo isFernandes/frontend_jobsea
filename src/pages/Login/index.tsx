4d4d87fc351f7c04fbbfa3f33cb5bc43abd21866
isFernandes <iago.stosfernandes@gmail.com>
2020-12-08 02:20:23 -0300

    first commit

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,36 +1,39 @@
 import React from "react";
 import styled from "styled-components";
-import './index.css';
-import Button from "@material-ui/core/Button";
-import TextField from "@material-ui/core/TextField";
+// import TextField from "@material-ui/core/TextField";
 import { Link } from "react-router-dom";
+
+import "./index.css";
+import Navbar from "../../components/Navbar";
+import imgBackground from "../../assets/HomePage/fundo@72x.png";
+import logotipeMain from "../../assets/HomePage/logotipo-caravela@72x.png";
+import lemeRodape from "../../assets/HomePage/leme-rodape@72x.png";
 
 function Login() {
   return (
     <Container>
-      <Form action="POST" method="post">
-        <LoginField>
-          <h4>Entre</h4>
-          <TextField type="email" label="E-mail"></TextField>
-          <TextField
-            id="standard-basic"
-            type="password"
-            label="Senha"
-          ></TextField>
-          <ButtonArea>
-            <Button className="button" color="primary">
-              <Link className="linking" to="/esqueceu-senha">Esqueceu a Senha?</Link>
-            </Button>
-            <Button className="button" variant="contained" color="primary" type="submit">
-              <Link className="linking-button" to="/feed">Entrar</Link>
-            </Button>
-          </ButtonArea>
-        </LoginField>
-        <blockquote>
-          <h4>Cadastre - se</h4>
-          <p>Não está cadastrado? Realize seu cadastro agora! <Link className="linking" to="/signup">Clique aqui</Link> </p>
-        </blockquote>
-      </Form>
+      <ImageBackground src={imgBackground} />
+      <Navbar placeholder="Busque um freelancer ..." title="J O B S E A" />
+      <LoginField>
+        <Logo src={logotipeMain} />
+      <ButtonArea>
+        <button className="create-job" >
+          <Link style={{color: '#fff'}} className="linking-button" to="/esqueceu-senha">
+            PUBLIQUE UM JOB
+          </Link>
+        </button>
+        <button
+          className="search-job"
+        >
+          <Link style={{color:'#3c7380'}} className="linking-button" to="/feed">
+            ENCONTRE UM JOB
+          </Link>
+        </button>
+      </ButtonArea>
+      <p>ENCONTRE SEU JOB NESSE MAR DE OPORTUNIDADES . . .</p>
+      <Leme className="leme-icon" src={lemeRodape} />
+      </LoginField>
+      
     </Container>
   );
 }
@@ -42,23 +45,33 @@ const Container = styled.div`
   justify-content: center;
   align-items: center;
   flex: 1;
+  flex-direction: column;
+  height: 100vh;
+  background-repeat: no-repeat, repeat;
+  background-color: #cccccc;
 `;
 
-const Form = styled.form`
-  display: flex;
-  flex-direction: row;
-  flex: 1;
-  justify-content: space-around;
-  align-items: center;
-  padding: 5px;
-  margin: 0 10rem 0 10rem;
+const ImageBackground = styled.img`
+  position: absolute;
+  z-index: 0;
+  height: 100%;
+  width: 100%;
 `;
+
+ const Leme = styled.img`
+   width: 60px;
+   height: 60px;
+   display: flex;
+   align-self: center;
+   margin-bottom: 10px;
+ `;
 
 const LoginField = styled.div`
   display: flex;
   flex-direction: column;
-  flex: 0.5;
+  flex: 1;
   justify-content: space-between;
+  z-index: 1;
 `;
 
 const ButtonArea = styled.div`
@@ -67,5 +80,16 @@ const ButtonArea = styled.div`
   flex-direction: row;
   justify-content: space-between;
   align-items: center;
-  flex: 1;
+  min-width: 80%;
+  align-self: center;
+  padding: 5px;
+  flex:1;
+`;
+
+const Logo = styled.img`
+  margin-top: 15px;
+  width: 379px;
+  height: 350px;
+  display: flex;
+  align-self: center;
 `;
--- src/pages/SingUp/index.tsx
+++ src/pages/SingUp/index.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import styled from "styled-components";
-import "./index.css";
 import Button from "@material-ui/core/Button";
 import TextField from "@material-ui/core/TextField";
 import { Link } from "react-router-dom";
@@ -8,9 +7,15 @@ import InputLabel from "@material-ui/core/InputLabel";
 import MenuItem from "@material-ui/core/MenuItem";
 import Select from "@material-ui/core/Select";
 
+import "./index.css";
+import imgBack from "../../assets/Signup/bg mar barco_Prancheta 1.png";
+import Navbar from "../../components/Navbar";
+
 function SingUp() {
   return (
     <Form action="POST" method="post">
+      <Navbar placeholder="Busque um freelancer ..." title="JOB SEA" />
+      <ImageBackground src={imgBack} />
       <Content>
         <h4>CADASTRE-SE</h4>
         <TextField
@@ -54,25 +59,38 @@ function SingUp() {
           }}
         />
 
-        <InputLabel id="demo-simple-select-label" className="inputLabel" >Função</InputLabel>
-        <Select className="select" labelId="demo-simple-select-label" id="demo-simple-select">
+        <InputLabel id="demo-simple-select-label" className="inputLabel">
+          Função
+        </InputLabel>
+        <Select
+          className="select"
+          labelId="demo-simple-select-label"
+          id="demo-simple-select"
+        >
           <MenuItem value={1}>Realizar projetos</MenuItem>
           <MenuItem value={2}>Publicar projetos</MenuItem>
           <MenuItem value={3}>Realizar e publicar projetos</MenuItem>
         </Select>
+
+        <ButtonArea>
+          <button
+            className="back-button"
+            color="primary"
+          >
+            <Link className="linking-text" to="/">
+              Voltar
+            </Link>
+          </button>
+          <button
+            className="done-button"
+            color="primary"
+          >
+            <Link style={{color:"#3c7380", fontWeight:"bold"}} to="/feed">
+              Pronto
+            </Link>
+          </button>
+        </ButtonArea>
       </Content>
-      <ButtonArea>
-        <Button style={{ marginRight: 20 }} variant="contained" color="primary">
-          <Link className="linking-text" to="/">
-            Voltar
-          </Link>
-        </Button>
-        <Button style={{ marginRight: 20 }} variant="contained" color="primary">
-          <Link className="linking-text" to="/feed">
-            Pronto
-          </Link>
-        </Button>
-      </ButtonArea>
     </Form>
   );
 }
@@ -85,16 +103,26 @@ const Form = styled.form`
   flex: 1;
   justify-content: space-around;
   align-items: center;
-  padding: 5px;
-  height: 98.5%;
+  min-height: 100%;
+  z-index: 1;
 `;
 
 const Content = styled.div`
   flex: 5;
   flex-direction: column;
   display: flex;
-  width: 600px;
+  width: 565px;
   align-items: center;
+  background-color: #3c7380;
+  z-index: 1;
+  opacity: 0.90;
+`;
+
+const ImageBackground = styled.img`
+  position: absolute;
+  z-index: 0;
+  height: 100vh;
+  width: 100vw;
 `;
 
 const ButtonArea = styled.div`
