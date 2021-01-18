2a2059a077782509d1fe88427ff153a9077de609
isFernandes <iago.stosfernandes@gmail.com>
2021-01-18 20:55:54 -0300

    iniciando paginas relacionadas a projeto

--- src/pages/ProjectCreate/index.tsx
+++ src/pages/ProjectCreate/index.tsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import styled from "styled-components";
 
 import './index.css';
 
 const ProjectCreate: React.FC = () => {
-  return <div />;
+  return(
+    <Container></Container>
+  );
 }
 
 export default ProjectCreate;
+
+const Container = styled.div`
+
+`;
