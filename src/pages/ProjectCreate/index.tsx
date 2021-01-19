160d4fb7fccb71b79f0763f41c407ed58067261a
isFernandes <iago.stosfernandes@gmail.com>
2021-01-18 21:35:45 -0300

    create project screen

--- src/pages/ProjectCreate/index.tsx
+++ src/pages/ProjectCreate/index.tsx
@@ -1,16 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from "styled-components";
+import InputDefault from '../../components/InputDefault';
+import Navbar from '../../components/Navbar';
 
 import './index.css';
 
 const ProjectCreate: React.FC = () => {
-  return(
-    <Container></Container>
+  const [nameProjeto, setNameProjeto] = useState("");
+  const changeName = (nome: string) => {
+    setNameProjeto(nome);
+  }
+
+  return (
+    <>
+      <Navbar title="PROJETOS" placeholder="Busque projetos" route="/" />
+      <Container>
+        <Content>
+          <InputDefault placeholder="Nome do projeto" newValue={changeName} />
+          <InputDefault placeholder="Caracteristicas Tecnicas" newValue={changeName} />
+          <InputDefault placeholder="Descricao" newValue={changeName} />
+          <InputDefault placeholder="Tempo estimado" newValue={changeName} />
+          <InputDefault placeholder="Valor disponivel" newValue={changeName} />
+        </Content>
+      </Container>
+    </>
   );
 }
 
 export default ProjectCreate;
 
 const Container = styled.div`
+
+`;
+const Content = styled.form`
 
 `;
